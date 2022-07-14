07d507a67da37b277b6bfc818af679f891b02331
DeniseBarbosa <deniseluvoir@gmail.com>
2022-07-14 16:11:35 -0300

    fiz a remoção do menu contato e sobre

--- receitas/views.py
+++ receitas/views.py
@@ -1,17 +1,7 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
 
-    return HttpResponse('Pagina inicial Denise')
-
-
-def sobre(request):
-
-    return HttpResponse('Pagina sobre o site da Denise')
-
-
-def contato(request):
-
-    return HttpResponse('Pagina de contatos da Denise')
+    return render(request, 'receitas/home.html')
